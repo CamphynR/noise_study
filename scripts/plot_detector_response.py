7bf6fc946ad40b40ff48e2638e658e94746c70e5
RNO-Gusers user <rubencamphyn@gmail.com>
2024-09-04 11:42:45 +0200

    fix bug in mask of multiple channels total plot

--- scripts/plot_detector_response.py
+++ scripts/plot_detector_response.py
@@ -14,7 +14,8 @@ if __name__ == "__main__":
                         default = 24)
     parser.add_argument("-c", "--channel",
                         type = int,
-                        default = None)
+                        nargs = "+",
+                        default = [])
     parser.add_argument("-y", "--year",
                         type = int,
                         default = 2023)
@@ -26,22 +27,22 @@ if __name__ == "__main__":
                             select_stations = args.station)
     det.update(datetime.datetime(args.year, 8, 1))
 
-    if args.channel is not None:
+    if len(args.channel) == 1:
         response = det.get_signal_chain_response(station_id = args.station, channel_id = args.channel)
-        fig, ax = response.plot(det.get_signal_chain_response(station_id = args.station, channel_id = args.channel))
+        fig, ax = response.plot()
     else:
-        responses = [det.get_signal_chain_response(station_id = args.station, channel_id = c) for c in range(7)]
+        responses = [det.get_signal_chain_response(station_id = args.station, channel_id = c) for c in args.channel]
         freq = np.linspace(0, 1.4) * units.GHz
         gains = [response(freq, component_names = "total") for response in responses]
         fig, ax = plt.subplots()
         for i, gain in enumerate(gains):
-            mask = gain > 0
-            ax.plot(freq[mask]/units.MHz, 20 * np.log10(np.abs(gain[mask])), label = f"total of channel {i}")
+            mask = np.abs(gain) > 0
+            ax.plot(freq[mask]/units.MHz, 20 * np.log10(np.abs(gain[mask])), label = f"total of channel {args.channel[i]}")
         ax.set_xlabel("freq / MHz")
         ax.set_ylabel("gain / dB")
         ax.legend(loc = 1)
 
     fig.suptitle(f"response station {args.station}, year {args.year}")
-    channel_name = f"channel{args.channel}" if args.channel is not None else "allchannels"
+    channel_name = f"channel{args.channel}".replace("[", "").replace("]", "").replace(", ", "_")
     fig_path = os.path.abspath(f"{os.path.dirname(__file__)}/../figures")
     fig.savefig(f"{fig_path}/response_station{args.station}_{channel_name}_year{args.year}.png") 
