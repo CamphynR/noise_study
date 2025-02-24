be05a02feece91f7d9df416d079fe43552eccf0e
Ruben Camphyn <rubencamphyn@gmail.com>
2025-02-24 08:20:12 +0100

    Fix fitting scripts

--- fitting/fit_spectrum.py
+++ fitting/fit_spectrum.py
@@ -1,5 +1,6 @@
 import argparse
-from iminuit import Minuit, LeastSquares
+from iminuit import Minuit 
+from iminuit.cost import LeastSquares
 import numpy as np
 
 from NuRadioReco.modules.io.NuRadioRecoio import NuRadioRecoio
@@ -12,40 +13,39 @@ def read_freq_spec_file(path):
     result_dictionary = read_pickle(path)
     frequencies = result_dictionary["freq"]
     frequency_spectrum = result_dictionary["frequency_spectrum"]
-    return frequencies, frequency_spectrum
+    var_frequency_spectrum = result_dictionary["var_frequency_spectrum"]
+    return frequencies, frequency_spectrum, var_frequency_spectrum
 
 
-def fit_gain_factor(ice_spec, el_spec, gal_spec, gain):
-    return gain * (ice_spec + el_spec + gal_spec)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--data", "-d", help="path to dat pickle file")
-    parser.add_argument("--ice_sim", "-ice", help="path to ice pickle files")
-    parser.add_argument("--electronic_sim", "-el", help="path to electronic pickle files")
-    parser.add_argument("--galactic_sim", "-gal", help="path to galactic pickle files")
+    parser.add_argument("--sim", "-s", help="path to summed sim pickle files")
     args = parser.parse_args()
 
-    data_dic = read_freq_spec_file(args.data)
-    ice_dic = read_freq_spec_file(args.ice_sim)
-    el_dic = read_freq_spec_file(args.electronic_sim)
-    gal_dic = read_freq_spec_file(args.galactic_sim)
+    frequencies, data_spectrum, var_data_spectrum = read_freq_spec_file(args.data)
+    sum_frequencies, sum_spectrum, _ = read_freq_spec_file(args.sim)
 
-    frequencies = data_dic["frequencies"]
-    assert np.all([dic["frequencies"]==frequencies for dic in [ice_dic, el_dic, gal_dic]])
+    assert np.all(sum_frequencies == frequencies)
 
+    channel_id = 0
+    x_data = frequencies
+    y_data = data_spectrum[channel_id]
+    y_err = var_data_spectrum[channel_id]
 
-    x_data = np.array([ice_dic["frequency_spectrum"],
-                       el_dic["frequency_spectrum"],
-                       gal_dic["frequency_spectrum"]
-                       ])
-
-    y_data = np.array([data_dic["frequency_spectrum"]])
-
+    def fit_gain_factor(freq, gain):
+        spectrum = sum_spectrum[channel_id]
+        index = np.nonzero(freq == frequencies)
+        return gain * np.squeeze(spectrum[index])
 
     fit_function = fit_gain_factor
-    cost_function = LeastSquares(x_data, y_data, fit_function)
+    cost_function = LeastSquares(x=x_data, y=y_data,
+                                 yerror=y_err,
+                                 model=fit_function)
 
     m = Minuit(cost_function, gain=1.)
     m.migrad()
+    m.hesse()
+    print(m.params)
